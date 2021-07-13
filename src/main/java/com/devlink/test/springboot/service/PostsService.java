b8b349868c5231490ca0e964305a345fcc4d8788
mkjeon <mkjeon@hanfa.co.kr>
2021-07-13 18:08:52 +0900

    Posts save, update

--- src/main/java/com/devlink/test/springboot/service/PostsService.java
+++ src/main/java/com/devlink/test/springboot/service/PostsService.java
@@ -2,12 +2,16 @@ package com.devlink.test.springboot.service;
 
 import com.devlink.test.springboot.domain.posts.Posts;
 import com.devlink.test.springboot.domain.posts.PostsRepository;
+import com.devlink.test.springboot.web.dto.PostsListResponseDto;
 import com.devlink.test.springboot.web.dto.PostsResponseDto;
 import com.devlink.test.springboot.web.dto.PostsSaveRequestDto;
 import com.devlink.test.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -30,5 +34,12 @@ public class PostsService {
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
